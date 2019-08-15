786cbc2122ba35b99dd1c2c11264576ca4bbef48
Luis gutierrez <alejandroluis444@gmail.com>
2019-08-15 18:02:50 -0500

    Mysql

--- miappotro/admin.py
+++ miappotro/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Libro
+from .models import *
 
 admin.site.register(Libro)
+admin.site.register(Prestamo)
+admin.site.register(Ejemplar)
+admin.site.register(DetallePrestamo)
--- miappotro/models.py
+++ miappotro/models.py
@@ -5,3 +5,22 @@ class Libro(models.Model):
     Descripcion = models.CharField(max_length=45)
     ISBN = models.CharField(max_length=45)
 
+    def __str__ (self):
+        return self.Nombre
+
+class Prestamo(models.Model):
+    Fechaprestamo = models.DateField(null=False)
+    NombreCliente = models.CharField(max_length=45)
+    TelefonoCliente = models.CharField(max_length=45)
+    Estado = models.BooleanField(null=False)
+
+class Ejemplar(models.Model):
+    NumeroEjemplar = models.IntegerField()
+    FechaCompra = models.DateField(null=False)
+    Libro = models.ForeignKey(Libro, on_delete=models.CASCADE)
+
+class DetallePrestamo(models.Model):
+    Prestamo = models.ForeignKey(Prestamo, on_delete=models.CASCADE)
+    Ejemplar = models.ForeignKey(Ejemplar, on_delete=models.CASCADE)
+
+
